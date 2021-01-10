54d74837adac3e72f88570943c51186ab119405a
Santosh Gawde <s.gawde@nfer.ac.uk>
2021-01-10 12:51:47 +0000

    creating a navbar for the application

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [PageNotFoundComponent],
+  declarations: [PageNotFoundComponent, NavBarComponent],
+  exports:[NavBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class CoreModule { }
